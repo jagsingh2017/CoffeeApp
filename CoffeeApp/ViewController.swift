e54382c5256d3b047e4f17ecefdb43e15b5cb37b
jagsingh2017 <scorpiolibra78@gmail.com>
2016-12-20 22:14:26 -0800

    My first Commit for Coffee App

--- CoffeeApp/ViewController.swift
+++ CoffeeApp/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var inputText: UITextField!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    @IBOutlet weak var payPrice: UILabel!
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        inputText.layer.cornerRadius = 15.0
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismisskeyboard))
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func dismisskeyboard() {
+        view.endEditing(true)
     }
 
-
+    @IBAction func calculateTapped(_ sender: Any) {
+        
+        
+        if inputText.text == "" {
+               errorLabel.text = "Enter value in Box"
+        } else {
+            let input = Double(inputText.text!)
+        
+            payPrice.text = "$ \(input! * 5.0)"
+            
+            
+            dismisskeyboard()
+        }
+    
+        
+        
+    }
 }
 
